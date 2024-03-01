66a77d64b2fd44a7be9cf41e720f8e711ace9ba2
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-03-01 20:13:26 +0300

    loop

--- index.js
+++ index.js
@@ -1,10 +1,89 @@
+class Myslider {
+  constructor(selector, settings) {
+    this.settings = settings
+    this.$el = document.querySelector(selector) 
+    this.$slider = this.$el.querySelector("[data-myslider='slider']")
+    this.activeId = 0
+    this.slideWIdth = window.innerWidth / this.settings.slides
+    this.slidesCount = this.$slider.querySelectorAll("[data-myslider='slide']").length
+    this.sectionCount = Math.ceil(this.slidesCount / this.settings.slides)
+    this.dots = this.createDots()
+    this.position = this.$slider.style.left
+    this.sliderInit()
+  }
 
+  sliderInit() {
+    let index = 0
+    this.$slider.style.width = this.slideWIdth * this.slidesCount + 'px'
+    this.$slider.querySelectorAll("[data-myslider='slide']").forEach($slide => {
+      $slide.style.width =  this.slideWIdth + 'px'
+      $slide.dataset.mysliderid = index
+      index++
+    })
 
+    const $next = this.$el.querySelector("[data-myslider='next']")
+    $next.addEventListener('click', () => {
+      this.activateSlide(this.activeId + 1)
+    });
+
+    const $prev = this.$el.querySelector("[data-myslider='prev']")
+    $prev.addEventListener('click', () => {
+      this.activateSlide(this.activeId - 1)
+    });
+
+  }
+
+  createDots() {
+    const $dots = this.$el.querySelector("[data-myslider='dots']")
+    for (let i = 0; i < this.sectionCount; i++) {
+      $dots.insertAdjacentHTML('beforeend', `<div class="myslider__dots__button" data-mysliderdot="${i * (this.settings.slides-1)}" data-myslider='dot'></div>`)
+    }
+    document.querySelector("[data-myslider='dot']").classList.add('active')
+    return $dots
+  }
+
+  activateSlide(n) {
+    if (n < 0) {
+      this.position = this.slideWIdth * (this.slidesCount - this.settings.slides)
+      this.$slider.style.left = -this.position + 'px'
+      this.activeId = this.slidesCount - this.settings.slides
+     } else {
+      if (n < this.slidesCount - (this.settings.slides - 1)) {
+        this.position = this.slideWIdth * n
+        this.$slider.style.left = -this.position + 'px'
+        this.activeId = n
+       } else {
+        this.$slider.style.left = 0
+        this.activeId = 0
+       }
+     }
+
+     console.log(this)
+  }
+
+}
+
+const slider = new Myslider("[data-myslider='slidercontainer']", {
+  slides: 3
+})
+console.log(slider)
+
+/* 
 document.querySelectorAll("[data-myslider='slidercontainer']").forEach($sliderwrapper => {
   const settings = {
     slides: 3,
     dots: true,
     swipe: true
+  }
+  const state = {
+    currentSlide: 0,
+    currentSection: 1,
+    sectionRest: 0,
+    slidesRest: 0,
+    slidesPrev: 0,
+    slidesCount: 0,
+    sectionsCount: 0,
+    slidesOver: 0
   }
   const $slider = $sliderwrapper.querySelector("[data-myslider='slider']")
   let slideWIdth =  window.innerWidth / settings.slides
@@ -18,36 +97,76 @@ document.querySelectorAll("[data-myslider='slidercontainer']").forEach($sliderwr
     index++
   })
 
+
+  state.sectionsCount = Math.ceil(slidesCount / settings.slides)
+
+  state.sectionRest = Math.floor(slidesCount / settings.slides)
+
+  state.slidesOver = slidesCount - Math.ceil(slidesCount / state.sectionRest)
+
   let position = $slider.style.left
-  let sliderRest = slidesCount - settings.slides
-  let currentSlideId = 0
-  const move = (dir, step) => {
+  state.slidesRest = slidesCount - 1
+  console.log(state)
+  const move = (dir, step, isarrow = false) => {
       if (dir === 'next') {
-        if (sliderRest > 0) {
+        if (state.slidesRest > 0) {
+          if (isarrow && state.slidesCount >= settings.slides - 1 ) {
+            const $dotnext = document.querySelector("[data-myslider='dot'].active")
+            $dotnext.classList.remove('active')
+            $dotnext.nextElementSibling.classList.add('active')
+            state.currentSection += 1
+            state.slidesCount = 0
+          } else {
+            state.slidesCount += step
+
+          }
+          if (state.slidesRest < step) {
+            step -= state.slidesRest
+          }
           position = position - slideWIdth * step
-          sliderRest -= 1
-          currentSlideId += step
+          state.slidesRest -= step
+          state.currentSlide += step
+          state.slidesPrev += step
         }
+       
       }
       if (dir === 'prev') {
-        if (sliderRest < slidesCount - settings.slides) {
+        if (state.slidesPrev > 0 ) {
+          if (isarrow && state.slidesCount <= settings.slides - 1 && state.currentSection > 1) {
+            const $dotprev = document.querySelector("[data-myslider='dot'].active")
+            $dotprev.classList.remove('active')
+            $dotprev.previousElementSibling.classList.add('active')
+            state.currentSection -= 1
+            state.slidesCount = settings.slides - 1
+          }else {
+            state.slidesCount -= step
+
+          }
+          if (state.slidesRest < step) {
+            step -= state.slidesRest
+          }
           position = position + slideWIdth * step
-          sliderRest += 1
-          currentSlideId -= step
+          state.slidesRest += step
+          state.currentSlide -= step
+          state.slidesPrev -= step
+        
         }
+      
       }
+      console.log(state)
       $slider.style.left = position + 'px'
     }
 
 
+
   const $next = $sliderwrapper.querySelector("[data-myslider='next']")
   $next.addEventListener('click', () => {
-    move('next', 1)
+    move('next', 1, true)
   });
 
   const $prev = $sliderwrapper.querySelector("[data-myslider='prev']")
   $prev.addEventListener('click', () => {
-    move('prev', 1)   
+    move('prev', 1, true)   
   });
 
   if (settings.dots) {
@@ -69,9 +188,12 @@ document.querySelectorAll("[data-myslider='slidercontainer']").forEach($sliderwr
             $el.classList.add('active')
 
             if (currentId > slideId) {
-              move('prev', currentId - slideId)   
+              move('prev', currentId - slideId, false)  
+              state.currentSection -= 1
+             
             } else {
-              move('next', slideId - currentId)   
+              move('next', slideId - currentId, false)   
+              state.currentSection += 1
             } 
 
           }
@@ -131,4 +253,4 @@ document.querySelectorAll("[data-myslider='slidercontainer']").forEach($sliderwr
  
 
 
-})
+}) */
